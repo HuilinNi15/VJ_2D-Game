21d7e2f4b681eb5ca9a073f0a56ae4a0a767399b
VictorGesiarz <victor.gesiarz@estudiantat.upc.edu>
2024-10-17 14:59:39 +0200

    Forest map

---
+++ CastleOfIllusions/CastleOfIllusions/images/tiles_forest_practice_map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles_forest_practice_map" tilewidth="16" tileheight="16" tilecount="32" columns="8">
+ <image source="tiles_forest_practice_map.png" width="128" height="64"/>
+</tileset>
